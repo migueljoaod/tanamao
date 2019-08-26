4bfa4c1db54b6e132e338181b78a3fa6e88c9c5d
miguel <miguel.joao@totvs.com.br>
2019-08-26 18:39:02 -0300

    Removing footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ItemListComponent } from './components/item-list/item-list.component';
+import { UserAdsComponent } from './user-ads/user-ads.component';
 
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'userads', component: ItemListComponent }
+    { path: 'userads', component: UserAdsComponent }
 ];
 
 @NgModule({
